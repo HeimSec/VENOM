852dd07d6466a12e6924b234ac2ecd812a9539a9
Jan Gebser <600613.m6@gmail.com>
2024-02-25 23:16:15 +0100

    Update VENOM.py

--- VENOM.py
+++ VENOM.py
@@ -1,5 +1,9 @@
 """
-Simple tryout of a Port Scanner
+CODENAME   :    VENOM.py
+DESCRIPTION:    Simple tryout of a Port Scanner
+DEVELOPER  :    Jan Gebser (@Brainhub24)
+REPORSITORY:    https://github.com/HeimSec/VENOM
+
 
 I want to try to build a basic port scanner that can scans a target for open ports within the range of 1 to 65535.
 It should provide a minimalistic command-line interface for the user to enter the target network (IP or FQDN).
@@ -49,77 +53,118 @@ Traceback (most recent call last):
   File "/usr/lib/python3.10/threading.py", line 1567, in _shutdown
     lock.acquire()
 KeyboardInterrupt:
-
-
 """
-import socket
-import threading
+import sys
 import time
 from datetime import datetime
+import socket
+import argparse
+import pyfiglet
+import threading
 import itertools
-import sys
 
 __VENOM__ = "__main__"
+    
+def VENOM_ANSI_BANNER():
+    VENOM_CODENAME = pyfiglet.figlet_format("VENOM PORTSCAN", font="slant")
+    VENOM_CONTENT_COLOR = "\033[92m" + VENOM_CODENAME + "\033[0m"
+    print(VENOM_CONTENT_COLOR, end='')
 
-def multi_threading_scan_animation():
-    """
-    Function to display a scanning animation during the port scanning process.
-    """
+def VENOM_SCAN_ANIMATION():
     chars = "/—\|"
     for char in itertools.cycle(chars):
         sys.stdout.write('\rScanning... ' + char)
         sys.stdout.flush()
         time.sleep(0.1)
 
-def VENOM_TARGETS(target):
+def VENOM_TARGETS(target, VENOM_INFO_OUTPUT):
     """
     Function to scan open ports on a target and display the results.
     
-    :param target: The target IP address or the Fully Qualified Domain Name [FQDN] to scan.
+    Param target: The target IP address or the Fully Qualified Domain Name [FQDN] to scan.
                    https://url.webservice.digital/FQDN
     """
-    print("\n" + "-" * 50)
-    print("PORT SCANNER")
+    VENOM_ANSI_BANNER()
+
+    print("\n" + "-" * 75)
+    print("Unleash the Storm, Dominate the Ports!")
     print("A project by: https://www.heimdall-security.net")
+    print("-" * 75)
+
+    if not target and not VENOM_INFO_OUTPUT:
+        print("\nUsage: VENOM.py [-h] [-info] target\n")
+        print("Options:")
+        print("  -h, --help  show this help message and exit")
+        print("  -info       Display detailed information for open ports.")
+        sys.exit()
+
+    if VENOM_INFO_OUTPUT:
+        VENOM_DISPLAY_ALL_PORTS_INFO()
+    else:
+        print(f"Scanning Target: {target}")
+        print(f"Scanning started at: {str(datetime.now())}")
+        print("-" * 50)
+
+        try:
+            VENOM_SCAN_PROCESS = threading.Thread(target=VENOM_SCAN_ANIMATION)
+            VENOM_SCAN_PROCESS.start()
+
+            VENOM_UNLEASHED_PORTS = []
+            for port in range(1, 65536):
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.settimeout(1)
+                    result = s.connect_ex((target, port))
+                    if result == 0:
+                        sys.stdout.write('\rScanning... done!')
+                        sys.stdout.flush()
+                        print(f"\nPort {port} is open")
+
+                        VENOM_UNLEASHED_PORTS.append(port)
+
+            VENOM_SCAN_PROCESS.join()
+
+            if VENOM_INFO_OUTPUT:
+                VENOM_DISPLAY_ALL_PORTS_INFO(VENOM_UNLEASHED_PORTS)
+
+        except socket.gaierror:
+            print("\nHostname Could Not Be Resolved !!!")
+        except socket.error:
+            print("\nServer not responding !!!")
+
+def VENOM_DISPLAY_ALL_PORTS_INFO(VENOM_UNLEASHED_PORTS=None):
+    VENOM_MOST_WANTED = {
+        21: "FTP - File Transfer Protocol. Used for transferring files between a server and a client.",
+        22: "SSH - Secure Shell. Provides a secure channel over an unsecured network in a client-server architecture.",
+        23: "Telnet. A network protocol used on the Internet or local area networks to provide a bidirectional interactive text-oriented communication facility.",
+        25: "SMTP - Simple Mail Transfer Protocol. Used to send emails between servers.",
+        53: "DNS - Domain Name System. Resolves domain names to IP addresses.",
+        80: "HTTP - Hypertext Transfer Protocol. Used for transmitting hypertext over the Internet.",
+        110: "POP3 - Post Office Protocol version 3. Retrieves emails from a mail server.",
+        443: "HTTPS - Hypertext Transfer Protocol Secure. A combination of HTTP and a cryptographic protocol.",
+    }
+
+    print("\n" + "-" * 50)
+    print("Port Information Table")
+    print("-" * 50)
+    print("{:<10} {:<30} {:<}".format("Port", "Service", "Description"))
     print("-" * 50)
 
-    print(f"Scanning Target: {target}")
-    print(f"Scanning started at: {str(datetime.now())}")
-    print("-" * 50)
-
-    try:
-        animate_thread = threading.Thread(target=multi_threading_scan_animation)
-        animate_thread.start()
-
-        for port in range(1, 65536):
-            with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.settimeout(1)
-                result = s.connect_ex((target, port))
-                if result == 0:
-                    sys.stdout.write('\rScanning... done!')
-                    sys.stdout.flush()
-                    print(f"\nPort {port} is open")
-
-        animate_thread.join()
-
-    except socket.gaierror:
-        print("\nHostname Could Not Be Resolved !!!")
-    except socket.error:
-        print("\nServer not responding !!!")
+    if VENOM_UNLEASHED_PORTS is not None:
+        for VENOM_PORT_LISTENER in VENOM_UNLEASHED_VENOM_PORT_LISTENERS:
+            if VENOM_PORT_LISTENER in VENOM_MOST_WANTED:
+                service = VENOM_MOST_WANTED[VENOM_PORT_LISTENER].split(' - ')[0]
+                description = VENOM_MOST_WANTED[VENOM_PORT_LISTENER].split(' - ')[1]
+            else:
+                print(f"{port}       {'Unknown'}                     {'Unknown'}")
 
 if __VENOM__ == "__main__":
-    print("-" * 50)
+    parser = argparse.ArgumentParser(description="VENOM Port Scanner")
+    parser.add_argument("target", nargs="?", default=None, help="The target IP address or FQDN to scan.")
+    parser.add_argument("-info", action="store_true", help="Display detailed information for open ports.")
+    args = parser.parse_args()
 
-    while True:
-        try:
-            user_input = input("\nEnter the target: ")
+    try:
+        VENOM_TARGETS(args.target, args.info)
 
-            if user_input.lower() == "bye":
-                print("Exiting the program. Goodbye!")
-                break
-
-            target = socket.gethostbyname(user_input)
-            VENOM_TARGETS(target)
-
-        except Exception as e:
-            print(f"\nAn error occurred: {e}")
+    except Exception as e:
+        print(f"\nAn error occurred: {e}")
