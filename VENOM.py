aaafa5d150207880033fcf3e9a73fa224faa2dab
Jan Gebser <600613.m6@gmail.com>
2024-02-25 08:29:07 +0100

    Update VENOM.py
    First working solution including some issues that need to be fixed... ^^

--- VENOM.py
+++ VENOM.py
@@ -14,6 +14,43 @@ Scanning started at: [DATE] [TIMESTAMP]
 Scanning... done!
 Port 21 is [RESULT]
 
+
+First working version including some issues that need to be resolve:
+┌──(ironcore㉿LAB.ONE)-[~/Development/GitHub/VENOM]
+└─$ python3 VENOM.py
+--------------------------------------------------
+
+Enter the target network (IP or domain)
+or type 'Bye' to quit: fritz.box
+
+--------------------------------------------------
+PORT SCANNER
+A project by: https://www.heimdall-security.net
+--------------------------------------------------
+Scanning Target: 192.168.178.1
+Scanning started at: 2024-02-25 08:19:12.488290
+--------------------------------------------------
+Scanning... done!
+Port 21 is open
+Scanning... done!
+Port 53 is open
+Scanning... done!
+Port 80 is open
+Scanning... done!
+Port 443 is open
+Scanning... \^CTraceback (most recent call last):
+  File "~/Development/GitHub/VENOM/VENOM.py", line 84, in <module>
+Scanning... |    VENOM_TARGETS(target)
+  File "~/Development/GitHub/VENOM/VENOM.py", line 59, in VENOM_TARGETS
+    result = s.connect_ex((target, port))
+KeyboardInterrupt
+Scanning... |^CException ignored in: <module 'threading' from '/usr/lib/python3.10/threading.py'>
+Traceback (most recent call last):
+  File "/usr/lib/python3.10/threading.py", line 1567, in _shutdown
+    lock.acquire()
+KeyboardInterrupt:
+
+
 """
 import socket
 import threading
@@ -38,7 +75,8 @@ def VENOM_TARGETS(target):
     """
     Function to scan open ports on a target and display the results.
     
-    :param target: The target IP address or domain to scan.
+    :param target: The target IP address or the Fully Qualified Domain Name [FQDN] to scan.
+                   https://url.webservice.digital/FQDN
     """
     print("\n" + "-" * 50)
     print("PORT SCANNER")
